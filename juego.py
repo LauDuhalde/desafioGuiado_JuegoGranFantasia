75e357b925e4d3da9b479139cbee83513380db11
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-03-12 17:07:57 -0300

    Comentarios

--- juego.py
+++ juego.py
@@ -3,32 +3,40 @@ import random
 print("¡Bienvenido a Gran Fantasía!")
 nombre = input("Por favor indique nombre de su personaje:\n")
 
+#Se crea jugador (nueva instancia)
 jugador = Personaje(nombre)
 print(jugador.estado)
 
-
+#Se crea Orco (nueva instancia)
 orco = Personaje("Orco")
 
+#Se consulta probabilidad de ganar
 probabilidad = jugador.probabilidad_ganar(orco)
 
+#Se usa método estatico para entregar información y pedir opción
 opcion_ingreso = Personaje.dialogo(probabilidad)
 
+#Juega hasta que escoja la opción 2 (Huir)
 while opcion_ingreso == 1:
-    
+
     atacar = random.uniform(0.0,1.0)
 
     if(atacar <= probabilidad):
         print("""¡Le has ganado al orco, felicidades!
 ¡Recibirás 50 puntos de experiencia!""")
+        #Setter
         jugador.estado = 50
         orco.estado =-30
     else:
         print("""¡Oh no! ¡El orco te ha ganado!
 ¡Has perdido 30 puntos de experiencia!""")
+        #Setter
         jugador.estado = -30
         orco.estado =50
+        
+    #Getter
     print(jugador.estado)
     print(orco.estado)
     
-    
+    #Se usa método estatico para volver a entregar información y pedir opción
     opcion_ingreso = Personaje.dialogo(probabilidad)
--- personaje.py
+++ personaje.py
@@ -5,10 +5,11 @@ class Personaje:
         self.experiencia = 0
     @property   
     def estado(self):
-        return f"""NOMBRE: {self.nombre}     NIVEL:      {self.nivel}     EXP: {self.experiencia}"""
+        return f"""NOMBRE: {self.nombre}     NIVEL: {self.nivel}     EXP: {self.experiencia}"""
     
     @estado.setter
     def estado(self, exp):
+        
         experiencia = self.experiencia + exp #Suma experiencia actual mas la ganada (o perdida)
         cambio_nivel = experiencia // 100 #puede ser positivo o negativo, es decir, subir o bajar nivel
         resto_exp = experiencia % 100 #Devuelve el resto de puntos que se debe almacenar
@@ -21,6 +22,7 @@ class Personaje:
             
         if(self.nivel<=0): #Si al cambiar de nivel se queda en 0 o negativo, se asigna nivel minimo (1)
             self.nivel = 1
+            self.experiencia = 0
             
     def __eq__(self, other):
         return self.nivel == other.nivel
